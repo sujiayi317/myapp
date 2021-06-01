a8617221d66c9fb05f653e40e5cc0fc2f5dfac7e
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-05-31 23:28:46 -0400

    Add comment

--- square.js
+++ square.js
@@ -1,3 +1,10 @@
+// In fact, exports is just a variable that gets initialized to the value of 
+// module.exports before the module is evaluated. That value is a reference 
+// to an object (empty object in this case). 
+// This means that exports holds a reference to the same object 
+// referenced by module.exports. It also means that by assigning another value 
+// to exports it's no longer bound to module.exports.
+
 // exports.area = function(width) { return width * width; };
 // exports.perimeter = function(width) { return 4 * width; };
 
