7b22d185680c587029b128b125e25921f448ea18
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-06-01 20:18:24 -0400

    Add middleware

--- index.js
+++ index.js
@@ -2,39 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+const members = require('./Members');
 
-
-const members = [
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'john@gmail.com',
-    status: 'active'
-  },
-  {
-    id: 2,
-    name: 'Bob Williams',
-    email: 'bob@gmail.com',
-    status: 'inactive'
-  },
-  {
-    id: 3,
-    name: 'Shannon Jackson',
-    email: 'shannon@gmail.com',
-    status: 'active'
-  }
-];
-
-
+// get all members: test with Postman
 app.get('/api/members', (req, res) => {
   res.json(members);
 });
 
+
+const logger = (req, res, next) => {
+  console.log('Hello');
+  next();
+};
+
+// init middleware
+app.use(logger);
+
+// birds Routes
 var birds = require('./birds');
+app.use('/birds', birds)
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// square functions are exported
 const square = require('./square');
 console.log('The area of a square with a width of 4 is ' +  square.area(4));
 
@@ -49,5 +40,5 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
 
-app.use('/birds', birds)
+
 
