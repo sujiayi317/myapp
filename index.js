c4a5d33aa22e888bdc7818bf363e69705c1428d3
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-06-01 22:03:13 -0400

    Get a single member, 404 error if not found

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ app.get('/api/members', (req, res) => {
   res.json(members);
 });
 
+// get a single member
+app.get('/api/members/:id', (req, res) => {
+  // res.send(req.params.id);
+
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+  
+
+})
+
 // init middleware
 app.use(logger);
 
