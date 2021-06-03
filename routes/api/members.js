e8dd87bd2d2d071bb399a52830de7f09a5c461cb
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-06-03 09:33:10 -0400

    Update and delete member

--- routes/api/members.js
+++ routes/api/members.js
@@ -40,4 +40,43 @@ router.post('/', (req, res) => {
     res.json(members);
 });
 
+// update member
+router.put('/:id', (req, res) => {
+    // res.send(req.params.id);
+  
+    const found = members.some(member => member.id === parseInt(req.params.id));
+  
+    if (found) {
+        //   res.json(members.filter(member => member.id === parseInt(req.params.id)));
+        const updMember = req.body;
+        members.forEach(member => {
+            if (member.id === parseInt(req.params.id)) {
+                member.name = updMember.name ? updMember.name : member.name;
+                member.email = updMember.email ? updMember.email : member.email;
+
+                res.json({ msg: 'Member updated', member })
+            }
+        });
+    } else {
+      res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+    }
+  
+}); 
+
+// delete a single member
+router.delete('/:id', (req, res) => {
+    // res.send(req.params.id);
+  
+    const found = members.some(member => member.id === parseInt(req.params.id));
+  
+    if (found) {
+        res.json({ 
+            msg: 'Member deleted', 
+        members: members.filter(member => member.id !== parseInt(req.params.id))});
+    } else {
+      res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+    }
+  
+}); 
+
 module.exports = router;
